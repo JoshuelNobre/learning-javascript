093582e65fc7770ab6bc494a79c83c994e8f2c7f
Joshuel <nobrej1999@gmail.com>
2022-03-02 17:26:07 -0300

    First commit

---
+++ guanabara/02-modulo-B/aula05.js
@@ -0,0 +1,7 @@
+var a = 200
+var B = 2000
+
+a
+B
+
+console.log(typeof a)
---
+++ rocketseat/01-first-steps/03-comentarios.js
@@ -0,0 +1,9 @@
+// Aqui tenho um comentário em linha
+// console.log('Bem vindos ao Starter')
+
+/* 
+Aqui tenho um comentário multi linha 
+aaaaaaaaaaaaaaaaaaaaaaa
+*/ 
+
+console.log('Olá, mundo!') // alguma coisa 
